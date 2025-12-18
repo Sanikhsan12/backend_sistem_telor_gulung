c2b4f51a132234b4c62287063221cf9ac89e4ab6
Sanikhsan12 <lordkazumasan12@gmail.com>
2025-12-19 02:39:08 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'admin@test.com'], 
+            [
+                'name' => 'Admin Gulung',
+                'password' => Hash::make('password'), 
+                'role' => 'admin',       
+                'status' => 'approved',  
+            ]
+        );
+
+        // 2. (Opsional) Buat 1 User Biasa buat tes
+        User::updateOrCreate(
+            ['email' => 'user@test.com'],
+            [
+                'name' => 'User Testing',
+                'password' => Hash::make('password'),
+                'role' => 'user',
+                'status' => 'approved',
+            ]
+        );
     }
 }
